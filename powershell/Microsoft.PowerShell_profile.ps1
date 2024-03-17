1aef0c0917455c425e937f8369f72985db1f6333
Lance M <158105893+lmohesky@users.noreply.github.com>
2024-03-17 11:27:20 -0500

    Create Microsoft.PowerShell_profile.ps1

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+winfetch
+Write-Host "Hello, Lance!" -ForegroundColor Green
+# oh-my-posh init pwsh --config 'D:\moh-cloud\Dev\catppuccin_macchiato.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\User\AppData\Local\Programs\oh-my-posh\themes\easy-term.omp.json' | Invoke-Expression
