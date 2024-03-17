556bcbcb12d8ec3f9ba9ba5b96fc87d8323aa8b4
Lance M <158105893+lmohesky@users.noreply.github.com>
2024-03-17 11:28:09 -0500

    Update Microsoft.PowerShell_profile.ps1
    Removed commented line

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,3 @@
 winfetch
 Write-Host "Hello, Lance!" -ForegroundColor Green
-# oh-my-posh init pwsh --config 'D:\moh-cloud\Dev\catppuccin_macchiato.omp.json' | Invoke-Expression
 oh-my-posh init pwsh --config 'C:\Users\User\AppData\Local\Programs\oh-my-posh\themes\easy-term.omp.json' | Invoke-Expression
